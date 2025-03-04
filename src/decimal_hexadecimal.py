ff625e1523ce785431ae6ff9d11ffe3d75d14069
Juan Camilo Suárez <ccamsoto@gmail.com>
2025-03-04 13:39:54 -0500

    upgrade: the decimal & hexadecimal conversion were corrected to accept the mantissa part of the numbers

--- src/decimal_hexadecimal.py
+++ src/decimal_hexadecimal.py
@@ -6,12 +6,12 @@ class Decimal_hexadecimal(ConvertInt):
         # decimal to hexadecimal
         dividendo = int(num_to_transform.split('.')[0]) # borra la mantisa
         residuo = ''
-        while dividendo > 0: # parte entera
-            if dividendo < 16:
+        while dividendo > 0: # evalúa la parte entera
+            if dividendo < 16: # evalúa los últimos 2 digitos
                 if dividendo >= 10: #con letras
                     residuo+=self.transformation_table(str(dividendo))
                 residuo+=str(dividendo)
-                break # falta la mantiza!            
+                break 
             
             modulo = dividendo %16
             if modulo >= 10 and modulo <=15:
@@ -24,30 +24,51 @@ class Decimal_hexadecimal(ConvertInt):
         # mantisa 
         if '.' in num_to_transform:
             index = num_to_transform.index('.')
-            mantisa = num_to_transform[index+1]
-            unir_matinsa = float(mantisa)*16
-            unir_matinsa = str(unir_matinsa)
-            hexadecimal+=f'.{unir_matinsa[0]}'
-        else:
-            pass
-        return hexadecimal
+            mantisa = int(num_to_transform[index+1:]) #get the mantissa
+            while mantisa > 0: # evalúa la parte entera
+                if mantisa < 16: # evalúa los últimos 2 digitos
+                    if mantisa >= 10: #con letras
+                        residuo+=self.transformation_table(str(mantisa))
+                    residuo+=str(mantisa)
+                    break 
+                
+                modulo = mantisa %16
+                if modulo >= 10 and modulo <=15:
+                    residuo+=self.transformation_table(str(modulo)) #con letras
+                else:
+                    residuo += str(modulo) #sin letras
+                mantisa = mantisa//16 # sin mantiza     
+        mantisa_hexadecimal = residuo[::-1] #invierte el orden
+        return f'{hexadecimal}.{mantisa_hexadecimal}'
     
     def reverse_transform(self, num_to_transform:str) -> str:
         # hexadecimal to decimal
-        hexad = num_to_transform
-        decimal = 0
+        if '.' in num_to_transform:
+            hexad, mantisa = num_to_transform.split('.')
+        entire = 0
         n=0
         while len(hexad) != 0:
-            if hexad.isnumeric(): # get last num 
+            if hexad[-1].isnumeric(): # get last num 
                 digit= int(hexad[-1]) 
             else: # convert letters to nums
                 digit = int(self.transformation_table(hexad[-1])) 
-            decimal+= digit*(16**n)
+            entire+= digit*(16**n)
             hexad = hexad[:-1] # del last num
             n+=1
-        return str(decimal)
         
-    
+        # mantissa
+        n=0
+        mantisa_dec=0
+        while len(mantisa) != 0:
+            if mantisa[-1].isnumeric(): # get last num 
+                digit= int(mantisa[-1]) 
+            else: # convert letters to nums
+                digit = int(self.transformation_table(mantisa[-1])) 
+            mantisa_dec+= digit*(16**n)
+            mantisa = mantisa[:-1] # del last num
+            n+=1
+        return f'{entire}.{mantisa_dec}'
+            
     def transformation_table(self, num_to_transform:str) -> str:
         match num_to_transform:
             case "10": return "A"
